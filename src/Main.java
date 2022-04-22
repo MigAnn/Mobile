78202d53723894f09a844f2566cf1cf3a7d8888e
Анна <anakatkova166@yandex.ru>
2022-04-22 21:57:19 +0300

    Mobile

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 100;
+        int refill = 1000;
+        int sum = score + refill;
+        int bonus = refill / 100;
+        int change = bonus + sum;
+
+        if (refill < 1000) {
+            System.out.println("Ваш счёт пополнен на : " + sum);
+        }else {
+            System.out.println("Ваш счёт пополнен на : " + change);
+        }
+
+    }
+}
